54f37473190892e23b6a9e461ae1e7d60c429a5f
Lại Thanh Trúc <94795453+ThanhTruc04072003@users.noreply.github.com>
2023-11-16 14:20:44 +0700

    Add files via upload

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.6wu5gn9.mongodb.net',
+    DB_USER: 'Vilka',
+    DB_PASS: '083303000176',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: 'huynhthang9101',
+    JWT_EXPIRES: '36000000', // in milliseconds
+    EMAIL_USER: 'laithanhtruc0101@gmail.com', // gmail service
+    EMAIL_PASS: 'bhmj tumi olmv ubbr'
+  };
+  module.exports = MyConstants;
